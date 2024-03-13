e50d8e0fb33330d8311d54db09017187fb1fc526
ChristianBaciu <christian3110@outlook.it>
2024-03-13 16:10:59 +0100

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" data-bs-theme="dark">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    @vite('resources/js/app.js')
+</head>
+<body>
+    <h1 class="text-danger">Ciao Mondo!</h1>
+</body>
+</html>
